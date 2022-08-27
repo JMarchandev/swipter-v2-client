548ff5e2ce911240a9889bd20becd0b91c87348d
Jean-Marie MARCHAND <j.marchand.dev@gmail.com>
2022-08-27 13:42:45 +0200

    feat: home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 
 import Auth from "./pages/auth";
+import Home from "./pages/home";
 
 function App() {
   return (
     <>
       <style>{`
       body {
-        margin: 0
+        margin: 0;
+        background-color: #F0F0F0
       }
     `}</style>
       <Routes>
         <Route path="/auth" element={<Auth />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </>
   );
