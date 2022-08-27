c9c8303be5e65e4a0af99eb548c97b35f18c6ae1
Jean-Marie MARCHAND <j.marchand.dev@gmail.com>
2022-08-27 11:13:42 +0200

    feat: add tailwind

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,4 @@
-import App from './App';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
