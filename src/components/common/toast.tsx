9552c7b430a5ced322c3208aea5ce07d697632e8
Jean-Marie MARCHAND <j.marchand.dev@gmail.com>
2022-09-25 11:54:51 +0200

    feat: add toaster

---
+++ src/components/common/toast.tsx
@@ -0,0 +1,28 @@
+import { ToastIcon, Toaster, resolveValue } from "react-hot-toast";
+
+import { Transition } from "@headlessui/react";
+
+export const Toast = () => {
+  return (
+    <Toaster>
+      {(t) => (
+        <Transition
+          appear
+          show={t.visible}
+          className="transform p-4 flex bg-white rounded shadow-lg w-full"
+          enter="transition-all duration-150"
+          enterFrom="opacity-0 scale-50"
+          enterTo="opacity-100 scale-100"
+          leave="transition-all duration-150"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-75"
+        >
+          <ToastIcon toast={t} />
+          <p className="px-2">{resolveValue(t.message, t)}</p>
+        </Transition>
+      )}
+    </Toaster>
+  );
+};
+
+export default Toast;
